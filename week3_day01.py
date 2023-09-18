b66a337f05d82207199a39aa0b6ae06a0b6e98f6
vv5700 <vv5700@g.rit.edu>
2023-09-18 13:45:00 +0200

    Initial commit

---
+++ week3_day01.py
@@ -0,0 +1,40 @@
+
+import math
+
+def circle_area(radius):
+    ##not using radius = input...
+    #^ this does not work
+    #**2 works
+    area = 3.14159 * (radius * radius)
+    return area ##function is returning area value
+
+def my_sqrt(x):
+    return math.sqrt(x)
+
+    def hypotenuse(a,b):
+        a2 = math.pow(a,2)
+        b2 = math,pow(b,2)
+        return 
+
+def main():
+    #RETURN part
+    '''
+    area2 = circle_area(10)
+    print("Circle area is:",area2)
+    print("SQRT of number 5 is:", my_sqrt(5))
+    print("Hypotenuse of 10 and 10 is:",hypotenuse(10,10))
+    '''
+
+    ##BOLEAN
+    isOk = True
+    print(not isOk)
+    a = True
+    b = False
+    print(a and b)
+    print(a or b)
+    print(a ^ b)
+
+    if __name__ =="__main__":
+        main()
+
+
